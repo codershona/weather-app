7e713f93826107d7f8369e38210786909067dd83
codershona <falgunimst95@gmail.com>
2020-06-18 12:29:49 +0200

    Making HTTP Requests

--- README.md
+++ README.md
@@ -9,6 +9,13 @@
 ```
    - Run node app.js ;
    - Call Stack --> Node APIs ---> (Event Loop) Callback Queue ;
+   - Search in browser : http://api.weatherstack.com/current?access_key=5d603d1b7243e2be9be5afe01e214167&query=37.8267,-122.4233 ;
+   - npm init ;
+   - npm init -y ;
+   - npm i request@2.88.0 ;
+   - node app.js ;
+   - node app.js ;
+   - Notes : ( If you use darksky Api key(old) --> (console.log(data.currently) OR, If you use weatherstack (new) ---> (console.log(data.current)) ;
    - 
 
 
--- app.js
+++ app.js
@@ -1,89 +1,119 @@
-console.log('Staring')
+const request = require('request')
 
 
-setTimeout(() => {
-	console.log('2 Second Timer')
+const url = 'http://api.weatherstack.com/current?access_key=5d603d1b7243e2be9be5afe01e214167&query=37.8267,-122.4233'
 
-}, 2000)
-
-setTimeout(() => {
-	console.log('0 Second Timer')
-
-}, 0)
-
-// method 1 :
-// const x = 1
-
-// const y = x + 2
-
-// console.log('Sum is ' + y)
-
-// Call Stack ----> 
-         // console.log('Phillips')
-         // anonymous('Phillips')
-            // forEach(...)
-             // listLocations([...])
-             //   main()
-
-             // Results: Phillips
-             
-
-// method 2:
-// const listLocations = (locations) => {
-	// locations.forEach((location) => {
-      // console.log(location)
-
-		// })
-
-// }
-
-// const myLocations = ['Phllips', 'NYC']
-
-// listLocations(myLocations)
+request({ url: url }, (error, response) => {
+	// console.log(response)
+	const data = JSON.parse(response.body)
+	// console.log(data)
+	console.log(data.current)
 
 
-
-
-console.log('Stopping')
+})
 
 
 
 
 
-// method 3:
 
-// console.log('Strating up')
+
+
+
+
+
+
+
+
+
+
+
+// console.log('Staring')
+
 
 // setTimeout(() => {
-	// console.log('Two seconds')
+// 	console.log('2 Second Timer')
 
 // }, 2000)
 
 // setTimeout(() => {
-	// console.log('Zero seconds')
+// 	console.log('0 Second Timer')
 
 // }, 0)
 
-// console.log('Finishing up!')
+// // method 1 :
+// // const x = 1
 
-// Call Stack -->
+// // const y = x + 2
 
-    // console.log('Fin....') 
-    // console.log('Zero ....') 
+// // console.log('Sum is ' + y)
 
-    /// main()
+// // Call Stack ----> 
+//          // console.log('Phillips')
+//          // anonymous('Phillips')
+//             // forEach(...)
+//              // listLocations([...])
+//              //   main()
 
-    // Callback (2 sec)
+//              // Results: Phillips
+             
+
+// // method 2:
+// // const listLocations = (locations) => {
+// 	// locations.forEach((location) => {
+//       // console.log(location)
+
+// 		// })
+
+// // }
+
+// // const myLocations = ['Phllips', 'NYC']
+
+// // listLocations(myLocations)
 
 
- // Node APIs ---> 
-
-    // setTimeout (2 sec)
-    // setTimeout (0 sec)
-
-// Callback Queue(Event Loop) --->
-
-    // Callback (0 sec)
 
 
- // Result : starting up
+// console.log('Stopping')
+
+
+
+
+
+// // method 3:
+
+// // console.log('Strating up')
+
+// // setTimeout(() => {
+// 	// console.log('Two seconds')
+
+// // }, 2000)
+
+// // setTimeout(() => {
+// 	// console.log('Zero seconds')
+
+// // }, 0)
+
+// // console.log('Finishing up!')
+
+// // Call Stack -->
+
+//     // console.log('Fin....') 
+//     // console.log('Zero ....') 
+
+//     /// main()
+
+//     // Callback (2 sec)
+
+
+//  // Node APIs ---> 
+
+//     // setTimeout (2 sec)
+//     // setTimeout (0 sec)
+
+// // Callback Queue(Event Loop) --->
+
+//     // Callback (0 sec)
+
+
+//  // Result : starting up
