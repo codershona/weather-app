a410130b80a68f80b1c791c1b6719eb8747d1078
codershona <falgunimst95@gmail.com>
2020-07-01 12:31:48 +0200

    Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,2 +1,32 @@
 console.log('Client side javscript file is loaded!!')
 
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+	response.json().then((data) => {
+		console.log(data)
+
+
+	})
+
+})
+
+
+// Another Api:
+
+// fetch('http://localhost:3000/weather?address=!').then(() => {
+	// fetch('http://localhost:3000/weather?address=').then(() => {
+
+fetch('http://localhost:3000/weather?address=boston').then(() => {
+   response.json().then((data) => {
+   	if (data.error) {
+   		console.log(data.error)
+
+   	} else {
+   		console.log(data.location)
+   		console.log(data.forecast)
+     
+   	}
+
+   })
+
+})
+
